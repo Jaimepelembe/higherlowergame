3c83a0ba2029f9333f993e479d75425f5f0a2b32
jaime <jaimitopele@gmail.com>
2026-07-16 21:20:11 +0200

    Feat: added the files art.py and gameData.py

---
+++ art.py
@@ -0,0 +1,21 @@
+logoHigherLower='''
+                                            
+                                                    
+ _____ _     _              __                      
+|  |  |_|___| |_ ___ ___   |  |   ___ _ _ _ ___ ___ 
+|     | | . |   | -_|  _|  |  |__| . | | | | -_|  _|
+|__|__|_|_  |_|_|___|_|    |_____|___|_____|___|_|  
+        |___|                                       
+
+'''
+
+#Ao colocar r antes das aspas o Python trata as barras invertidas como caracteres literais, sem tentar interpretar escapes. 
+logoVersus=r""" 
+ _____ _____ 
+|  |  |   __|
+|  |  |__   |
+ \___/|_____|         
+
+ """
+num = 1000000000
+print(f"{num:,}")  # Output: 1,000,000
